5ceff92b496aaf516caf169dec4b285ab7e690e0
Micheal Berkeley <54487168+mza1999@users.noreply.github.com>
2022-12-17 23:27:41 +0300

    added comments on code

--- neural_network.py
+++ neural_network.py
@@ -3,9 +3,11 @@ import random as rn
 import pickle
 
 
-
+# layer class 
 class Layer:
+    # init function
     def __init__(self, n_inputs, n_neurons, activation=None) -> None:
+        # initializing weights, biases etc...
         self.weights = (np.random.rand(n_inputs, n_neurons) * 6) - 3 
         self.biases = np.zeros((1, n_neurons))
         self.n_inputs = n_inputs
@@ -24,7 +26,7 @@ class Layer:
         self.weights = new_weights
         self.biases = new_biases
 
-    def forward(self, inputs):
+    def forward(self, inputs): # getting the output of the layer
         self.outputs = np.dot(inputs, self.weights) + self.biases
         if self.activation == "sigmoid":
             self.outputs = self.sigmoid(self.outputs)
@@ -38,9 +40,11 @@ class Layer:
             raise("no such activation function")
         return self.outputs
     
-    def backward(self, inputs, index, layers,y ,learning_rate):
-        
+    def backward(self, inputs, index, layers,y ,learning_rate): # making the layer learn
+        # first getting the output
         output = self.forward(inputs)
+
+        # calculating the learning rate & modify the weights & biases of the layer
 
         if index == 0:
             inps = np.zeros((len(inputs), 1))
@@ -65,36 +69,48 @@ class Layer:
             self.weights += delta * inputs
             self.biases += delta
 
-            # return next_delta , error
             return self.weights , error
         
 
 
-
-    def mutate(self, mutation_rate):
+    def mutate(self, mutation_rate): # mutating the weights and biases randomly
         for i in range(self.n_neurons):
             self.biases[0][i] += rn.uniform(-mutation_rate, mutation_rate)
             for j in range(self.n_inputs):
                 self.weights[j][i] += rn.uniform(-mutation_rate, mutation_rate)
     
-    def sigmoid(self,x):
+    def sigmoid(self,x): # sigmoid function
         return 1 / (1 + np.exp(-x))
     
-    def ReLU(self, x):
+    def ReLU(self, x): # ReLU function
         return np.maximum(0, x)
-    def softmax(self, x):
+
+    def softmax(self, x): # softmax function
         exp_values = np.exp(x - np.max(x, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         return probabilities
     
-    def tanh(self, x):
+    def tanh(self, x): # tanh function
         return 2*self.sigmoid(2*x)-1
 
 
 
-
+# this is the Neural Network model 
+# it contains the layers
 class Model:
     def __init__(self, shape=None, mutation_rate=0.01, activations=None) -> None:
+        # the shape is a tuple or any iterable object
+        # example: shape = (2, 3, 4, 3, 2) 
+        # this is the shape of the neural network
+
+        # the mutation rate is the rate at which the neural network can mutate 
+
+        # activations is a tuple or any iterable object
+        # it has to be of length len(shape) - 1 
+        # it must contain the list of strings of the activation functions for each layer
+        # available activations: ("sigmoid", "ReLU", "softmax", "tanh")
+
+
         self.mutation_rate = mutation_rate
         self.shape = shape
         self.layers = []
@@ -119,21 +135,21 @@ class Model:
                     index += 1
                 
         
-    def get_shape(self):
+    def get_shape(self): # getting the shape of the NN
         return self.shape
     
-    def predict(self, inputs):
+    def predict(self, inputs): # getting the output of the NN
         output = inputs
         for layer in self.layers:
             output = layer.forward(output)
         
         return output
     
-    def mutate(self):
+    def mutate(self): # mutate the NN
         for layer in self.layers:
             layer.mutate(self.mutation_rate)
     
-    def copy(self):
+    def copy(self): # returns a copy of the current Model
         copy_model = Model(self.shape, self.mutation_rate, self.activations)
         copy_layers = []
         for layer in self.layers:
@@ -141,11 +157,14 @@ class Model:
         copy_model.insert_layers(copy_layers)
         return copy_model
     
-    def insert_layers(self, layers):
+    def insert_layers(self, layers): # inserts an array of layers
         self.layers = layers
     
 
-    def train(self, X, y, learning_rate=0.01):
+    def train(self, X, y, learning_rate=0.01): # train the Model
+        # X: is the input
+        # y: is the correct values
+        # learning_rate: by default 0.01
         self.layers[0].backward(X, 0, self.layers, y, learning_rate)
         # output = self.predict(X)
         # error = output - y
@@ -177,7 +196,7 @@ class Model:
 
 
 
-    def save(self, name):
+    def save(self, name): # saves the model as a pickle file
         mdl = {}
         mdl['layers'] = {}
             
@@ -200,7 +219,7 @@ class Model:
         pickle_out.close()    
     
 
-    def load(self, name):
+    def load(self, name): # loads the model from a pickle value
         pickle_in = open(str(name)+".pickle", 'rb')
         mdl = pickle.load(pickle_in)
         pickle_in.close()
